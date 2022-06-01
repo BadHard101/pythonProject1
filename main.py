0b4cc50257f3a5752b1fbdbc2a5ff552ca491758
Bad Hard <badhard5693@gmail.com>
2022-06-01 12:36:13 +0300

    132

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
+from re import search
 import openpyxl
+from datetime import datetime
 from bs4 import BeautifulSoup
 page = requests.get("https://www.mirea.ru/schedule/")
 soup = BeautifulSoup(page.text, "html.parser")
@@ -26,4 +28,57 @@ for link in links:
 """
 book = openpyxl.load_workbook("kurs_1.xlsx")  # открытие файла
 sheet = book.active  # активный лист
-print(sheet['BN2'].value)
+
+user_message = "ИКБО-09-21"
+wek=0
+wekday=1
+list_par = []
+
+def shed_day(list_par, x, y, wek):
+    list_par = []
+    for col in sheet.iter_cols(min_row=2, max_col=sheet.max_column, max_row=2):
+        for cell in col:
+            if search(r'([А-Я]{4}-\d{2}-\d{2})', str(cell.value)):
+                if cell.value==user_message:
+                    colm=cell.column
+                    for row in range(x+wek,y,2):
+                        list_par.append(sheet.cell(row=row,column=colm).value)
+    return list_par
+
+
+
+
+if (wekday==0):
+    shed_day(list_par, 4, 15, wek)
+elif (wekday==1):
+    shed_day(list_par, 16, 27, wek)
+elif (wekday==2):
+    shed_day(list_par, 28, 39, wek)
+elif (wekday==3):
+    shed_day(list_par, 40, 51, wek)
+elif (wekday==4):
+    shed_day(list_par, 52, 63, wek)
+elif (wekday==5):
+    shed_day(list_par, 64, 75, wek)
+else:
+    list_par = []
+
+
+    def _get_number_week(day_today: datetime) -> int:
+        """Получение номера недели.
+
+        Parameters
+        ----------
+        day_today: datetime
+            сегодняшняя дата
+        Return
+        ----------
+        number: int
+            номер недели
+        """
+        first_week = Config.get_weeks_info()["start_week"].isocalendar()[1]
+        current_week = day_today.isocalendar()[1]
+        number = current_week - first_week + 1
+        return number
+
+print(self._get_number_week(datetime.today()))
